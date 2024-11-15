e1562a8128a156a3b3a3cac8302aa16ebfd82c97
Erick Gathinji <ericgathinji@gmail.com>
2024-11-15 10:51:03 +0300

    django forms

--- sacco/app_forms.py
+++ sacco/app_forms.py
@@ -3,9 +3,15 @@ from django import forms
 
 from sacco.models import Customer
 
+GENDER_CHOICES = {"Male": "Male", "Female": "Female"}
 
 class CustomerForm(forms.ModelForm): # ensure import is strictly django import forms
     class Meta:
         model = Customer
         fields = ['first_name', 'last_name', 'email', 'dob', 'weight', 'gender']
+        widgets = {
+            'dob': forms.DateInput(attrs={'type': 'date', 'min': '1980-01-01', 'max': '2005-12-31'}),
+            'weight': forms.NumberInput(attrs={'type': 'number', 'min': '35', 'max': '100'}),
+            'gender': forms.Select(choices=GENDER_CHOICES),
+        }
 
