08bbe3656f5a4e121ae94935b2e3add03c49a86c
Erick Gathinji <ericgathinji@gmail.com>
2024-11-18 09:24:16 +0300

    Admin

--- sacco/models.py
+++ sacco/models.py
@@ -25,7 +25,8 @@ class Customer(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} {self.gender}"
+        # return f"{self.first_name} {self.last_name} {self.gender}"
+        return f"{self.first_name} {self.last_name}"
 
     class Meta:
         db_table = 'customers'
